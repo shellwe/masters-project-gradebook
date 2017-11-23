2927cb52a51f4352c530c5c0a286479d86ec275a
shellwe <shellwe@gmail.com>
2017-11-23 07:07:34 -0600

    updated connection credentials
    updated connection credentials

--- includes/connectmsqli.php
+++ includes/connectmsqli.php
@@ -1,6 +1,6 @@
 <?php
 //para7.unl.edu
-$mysqli = new mysqli("para7.unl.edu", "shellwe", "terra1", "shellwe"); 
+$mysqli = new mysqli("para7.unl.edu", "shellwe", "password", "shellwe"); 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error; 
 }
